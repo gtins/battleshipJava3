20cd216d3dad800bd904bbcdaaf2e74c7e80c971
Gustavo <gu.martins@catolicasc.edu.br>
2023-06-26 22:39:47 -0300

    tirei o for inutil

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Tabuleiro Tabuleiro = new Tabuleiro(10);
-
-        for (int i = 0; i < 1; i++) {
-            Tabuleiro.imprimir();
-        }
+        Tabuleiro.imprimir();
     }
 }
