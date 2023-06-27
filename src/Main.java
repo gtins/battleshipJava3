c89fe5964434c8789487384f5e8d5619f209787e
Gustavo <gu.martins@catolicasc.edu.br>
2023-06-26 23:17:58 -0300

    posicionamento dinamico funcionando

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Tabuleiro Tabuleiro = new Tabuleiro(10);
-        Tabuleiro.posicionar(5,0,0,true);
-        Tabuleiro.posicionar(5,2,2,false);
+
+        for(int i = 0; i < 5; i++){
+            System.out.printf("Digite o tamanho do seu Navio: ");
+            int tamanho = Integer.parseInt(scanner.nextLine());
+            System.out.printf("Digite a linha inicial: ");
+            int linha = Integer.parseInt(scanner.nextLine());
+            System.out.printf("Digite a coluna inicial: ");
+            int coluna = Integer.parseInt(scanner.nextLine());
+            System.out.printf("Horizontal? (true ou false): ");
+            boolean horizontal = Boolean.parseBoolean(scanner.nextLine());
+            Tabuleiro.posicionar(tamanho, linha, coluna, horizontal);
+        }
         Tabuleiro.imprimir();
+
+//        Tabuleiro.posicionar(5,0,0,true);
+//        Tabuleiro.posicionar(5,2,2,false);
+//        Tabuleiro.imprimir();
     }
 }
