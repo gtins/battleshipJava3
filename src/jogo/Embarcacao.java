7be4197039e223166fae7b5723c4520464841b7e
gtins <111321003+gtins@users.noreply.github.com>
2023-07-06 16:29:04 -0300

    comentarios embarcaçao

--- src/jogo/Embarcacao.java
+++ src/jogo/Embarcacao.java
@@ -1,5 +1,4 @@
 package jogo;
-
 import java.net.CookieHandler;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -9,17 +8,17 @@ public class Embarcacao extends Elemento {
     private HashMap<Integer, Cordenada> posicoes = new HashMap<>(); //criaçao de um objeto do tipo hashmap de nome posicoes, novo hashmap
     private int forca; //parametro
 
-    public Embarcacao(int x, int y, int tamanho , String tipo, boolean horizontal) { // construtor de ibjetos embarcaçao
+    public Embarcacao(int x, int y, int tamanho , String tipo, boolean horizontal) { // construtor de objetos embarcaçao
         //recebe os parametros de cordanadam, tamanho, tipo e orientaçao
-        this.forca = tamanho; //decisao inteligente, o tipo é igual ao tamanho ainfal é o numero de celulas
+        this.forca = tamanho; //decisao inteligente, o tipo é igual ao tamanho afinal é o numero de celulas
         this.tipo = tipo; //tipo recebido vira o tipo
-        posicionar(x, y, tamanho, horizontal);
+        posicionar(x, y, tamanho, horizontal); //metodo abaixo
     }
 
     private void posicionar(int x, int y, int tamanho, boolean horizontal) { //precisa de x, y tamanho e orientaçao
         if(! horizontal) {
             for (int i = 0; i < tamanho; i++) {
-                Cordenada cordenada = new Cordenada(x + i, y, tipo); //objeto do tipo cordenada de nome cordenada usa do construtor da cordenada IR PARA CORDENADA PARA ENTENDER
+                Cordenada cordenada = new Cordenada(x + i, y, tipo); //objeto do tipo cordenada de nome cordenada usa do construtor da cordenada
                 //IMPORTANTE, incremento no X afinal horizontal = eixo X
                 posicoes.put(cordenada.hashCode(), cordenada); //coloca dentro do objeto hashmap posicoes o hashcode do objeto cordenada recem criado como chave e a cordenada como valor.
                 //importante, o hashcode é a chave e o X e Y são os valores. ou seja um valor aponta pra um par ordenado
@@ -40,7 +39,7 @@ public class Embarcacao extends Elemento {
 
     public String getPosicoes() {
         StringBuilder string = new StringBuilder();//novo objeto string builder
-        string.append(" ("); //coisa pra printar bonitinho
+        string.append(" ("); //identação
         for (Cordenada elemento: posicoes.values() //tudo daqui pra baixo é um foreach
         ) { //para cada valor em posicoes...
             string.append(" x: ");// excreva x:
@@ -48,69 +47,53 @@ public class Embarcacao extends Elemento {
             string.append(" y:");//escreva y:
             string.append(elemento.getY());//pegue o y
         }
-        string.append(" )");//coisa pra printar bonitinho
-        return string.toString();//tudo que tava ali em cima tava dando append no novo objeto string builder criado
-        //então agora ele tem tudo aquilo
+        string.append(" )");//identação 
+        return string.toString();//tudo que estava ali em cima estava dando append no novo objeto string builder criado
+        //então agora ele possui tudo aquilo
     }
 
     public int atirar(int x, int y) {//metodo atirar recebe um valor x e y
         boolean acertou =  acertou(x, y); //vai pro metodo acertar
         if( acertou ) {//se acertou foi true
-            diminuirForca(); //metodo de diminuir a vida logo abaixo, bem simples
+            diminuirForca(); //metodo de diminuir a vida (logo abaixo)
         }
-        return forca;
-        //VOLTA PRO MAIN
+        return forca;//retorna força atualizada
     }
 
-    /**
-     * retorna a força pós processamento, ou seja, reduzida
-     */
-    private void diminuirForca() {
-        forca--;
+    private void diminuirForca() {//diinui a vida do navio
+        forca--;//decremento na força
     }
 
-    /**
-     * Retorna TRUE se dentro do objeto hashmap posicoes existe a CHAVE igual ao hashcode de TIRO
-     * tiro é um objeto cordenada, que recebe o x e y do metodo que o invoca (acertou) e passa pelo construtor padrao de cordenada
-     * a logica de como a chave é adicionada em posicoes da em posicionar.
-     * basicamente compara, se forem os mesmos x e y isso aqui da true, se não não da
-     * retorna false se não tiver
-     * VOLTA PARA ATIRAR
-     * @param x como cordenada do array (linha)
-     * @param y como cordenada do array (coluna)
-     * @return true quando acertou
-     */
     public boolean acertou(int x, int y) {//método acertar recebe valores x e y
         Cordenada tiro = new Cordenada(x, y);//objeto do tipo cordenada de nome tipo, nova cordenada (com x e y do parametro acertou)
-        int chave = tiro.hashCode();
-        boolean acertou = posicoes.containsKey(chave);
-        if( acertou ) {
-            Cordenada elemento = posicoes.get(chave);
-            elemento.acertou();
+        int chave = tiro.hashCode(); //chave recebe o hashcode de tiro
+        boolean acertou = posicoes.containsKey(chave); //acertou verifica se em posicoes existe a chave, boleano true ou false 
+        if( acertou ) {//se acertou for true
+            Cordenada elemento = posicoes.get(chave);//elemento busca a chave
+            elemento.acertou();//executa o método acertou
         }
         return acertou;
     }
 
     public boolean temPosicao(int x, int y) {//método acertar recebe valores x e y
         Cordenada tiro = new Cordenada(x, y);//objeto do tipo cordenada de nome tipo, nova cordenada (com x e y do parametro acertou)
-        int chave = tiro.hashCode();
-        boolean tem = posicoes.containsKey(chave);
+        int chave = tiro.hashCode(); //chave recebe o hashcode de tiro
+        boolean tem = posicoes.containsKey(chave); //tem verifica se em posicoes existe a chave, boleano true ou false
         return tem;
     }
 
     public Cordenada getCordenada(int x, int y) {//método acertar recebe valores x e y
         Cordenada pos = new Cordenada(x, y);//objeto do tipo cordenada de nome tipo, nova cordenada (com x e y do parametro acertou)
-        int chave = pos.hashCode();
-        return  posicoes.get(chave);
+        int chave = pos.hashCode(); //chave recebe o hashcode de pos
+        return  posicoes.get(chave); //busca a chave de pos e retorna
     }
 
     public int getForca() {//getter da força
         return this.forca;
     }
 
-    public boolean afundou() {
+    public boolean afundou() { //boleano que indica se afundou e zera a força
         return forca == 0;
     }
 }
-
 
